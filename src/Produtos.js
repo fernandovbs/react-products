6234465f9af172529d891268ccb12d9224002599
Fernando Souza <fernandostorm1@gmail.com>
2018-05-27 20:40:26 -0300

    Add produtosHome component

--- src/Produtos.js
+++ src/Produtos.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import ProdutosHome from './ProdutosHome'
 
 export default class Produtos extends Component{
     render(){
-        return <h1>Products List</h1>
+        return (
+        <div className='row'>
+            <div className='col-md-2'>
+                <h3>Categories</h3>
+                <ul><li>Link for categories</li></ul>
+            </div>
+            <div className='col-md-10'>
+                <h1>Produtos</h1>
+                <Route exact path={this.props.match.url} component={ProdutosHome} />
+            </div>
+        </div>
+        )
     }
 }
---
+++ src/ProdutosHome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ProdutosHome =  () => (
+    <div>
+        <h2>Product details</h2>
+    </div>
+)
+
+
+export default ProdutosHome
