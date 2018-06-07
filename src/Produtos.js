4a94f37edd230e88797a80e18cc0190996df3de4
Fernando Souza <fernandostorm1@gmail.com>
2018-06-07 05:40:33 -0300

    render product categories menu

--- src/Produtos.js
+++ src/Produtos.js
@@ -15,21 +15,30 @@ export default class Produtos extends Component{
 
     componentDidMount() {
         axios
-        .get('localhost:3001/categorias')
+        .get('http://localhost:3001/categorias')
         .then(res => {
             this.setState({
                 categorias: res.data
             })
         })
     }
+
+    renderCategoria = (cat) => {
+        console.log(cat)
+        return <li><Link to={`/produtos/categorias/${cat.id}`}>{cat.categoria}</Link></li>
+    }
+
     render(){
         const { match } = this.props
+        const { categorias } = this.state
 
         return (
         <div className='row'>
             <div className='col-md-2'>
                 <h3>Categories</h3>
-                <Link to='produtos/categorias/1'>Category 1</Link>
+                <ul>
+                    { categorias.map( this.renderCategoria ) }
+                </ul>
             </div>
             <div className='col-md-10'>
                 <h1>Produtos</h1>
