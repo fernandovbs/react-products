0e2a083ef8943e7aeedb296e6ef25784b4ba4e50
Fernando Souza <fernandostorm1@gmail.com>
2018-06-10 22:50:16 -0300

    Make categorias component get data dinamically

--- src/Categoria.js
+++ src/Categoria.js
@@ -1,7 +1,50 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Categoria extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            produtos: [],
+            categoria: {}
+        }
+    }
+
+    loadData = (id) => {
+        axios
+        .get(`http://localhost:3001/produtos?categoria=${id}`)
+        .then(res => {
+            this.setState({
+                produtos: res.data
+            })
+        })        
+        axios
+        .get(`http://localhost:3001/categorias/${id}`)
+        .then(res => {
+            this.setState({
+                categoria: res.data
+            })
+        })
+    }
+
+    renderProduto = (produto) => {
+        return <p className="well">{produto.produto}</p>
+    }
+
+    componentDidMount(){
+        this.loadData(this.props.match.params.catId)
+    }
+    componentWillReceiveProps(newProps) {
+        this.loadData(newProps.match.params.catId)
+    }
     render(){
-        return <h1>Categorie {this.props.match.params.catId}</h1>
+//        return <h1>{this.state.categoria}</h1>
+        return (
+            <div>
+                <h1>{this.state.categoria.categoria}</h1>
+                {this.state.produtos.map(this.renderProduto)}
+            </div>
+        )
     }
 }
