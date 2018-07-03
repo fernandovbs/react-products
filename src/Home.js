a692b45d70ea5ff1ba6534705f446f15e36ef826
Fernando Souza <fernandostorm1@gmail.com>
2018-07-03 06:30:48 -0300

    Fix wrong css tag

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 
 export default class Home extends Component {
     render(){
-        return <h1>Welcome to the products manager</h1>
+        return <h1 style={{"textAlign": "center", "marginTop":"10px"}}>Welcome to the products manager</h1>
     }
 }
